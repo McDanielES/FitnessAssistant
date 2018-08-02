94a165d098effb848e83befd145420b678660d56
Eric McDaniel <McDanielES@gmail.com>
2018-08-01 21:51:00 -0500

    Add option (10) to not save changes to profile
    This option only appears when the user has made at least one change
    to their profile. When option 10 appears, the user has the option to
    not save their changes, forcing the file to be reread and refresh
    the local memory.

--- Profile.java
+++ Profile.java
@@ -7,13 +7,15 @@ public class Profile
   private UserAccount currentUser;
   private long        lastLogin;
   private FileReader  userDatabase;
+  private int         userProfileIndex;
 
   Profile(){}
-  Profile(UserAccount _currentUser, long _lastLogin, FileReader _userDatabase)
+  Profile(UserAccount _currentUser, long _lastLogin, FileReader _userDatabase, int _userProfileIndex)
   {
     currentUser  = _currentUser;
     lastLogin    = _lastLogin;
     userDatabase = _userDatabase;
+    userProfileIndex = _userProfileIndex;
   }
 
   public void start() throws FileNotFoundException
@@ -41,25 +43,38 @@ public class Profile
         FitnessAssistant.clearScreen();
       }
 
-      //// (2) "Update User Profile" Update Menu ////
+
+      /***************************************************************************************|
+      |                       (2) "Update User Profile" Update Menu                           |
+      |***************************************************************************************/
       else if (input == 2)
       {
         boolean continueUpdateProfile = true;
+        boolean profileChangeRequested = false;
         do
         {
           FitnessAssistant.clearScreen();
           int updateProfileInput = -1;
+
           System.out.print("\n\t---------------\n\t| Update Menu |\n\t---------------\n\nWhat would you like to update?\n\n\t1.) Name\n\t2.) Age"
-          + "\n\t3.) Height\n\t4.) Lifestyle\n\n\t9.) Save Changes and Exit\n\n***Reminder: you can update your weight from the profile menu***\n\nSelection: ");
+            + "\n\t3.) Height\n\t4.) Lifestyle");
+          if (profileChangeRequested)
+          {
+            System.out.print("\n\n\t9.) Save Changes and Exit\n\t10.) Exit Without Saving Changes\n\n***Reminder: you can update your weight from the profile menu***\n\nSelection: ");
+          }
+          else
+          {
+            System.out.print("\n\n\t9.) Exit Without Making Changes\n\n***Reminder: you can update your weight from the profile menu***\n\nSelection: ");
+          }
           try
           {
             updateProfileInput = Integer.parseInt(userInput.next());
           }
           catch (NumberFormatException nfe)
-          {
+          { // Nothing, the loop will reiterate and clear the screen.
           }
 
-          // (2) "Update User Profile", (1) "Update Name"
+          /////////////////////////       (2) "Update User Profile", (1) "Update Name"     /////////////////////////
           if (updateProfileInput == 1)
           {
             FitnessAssistant.clearScreen();
@@ -71,7 +86,9 @@ public class Profile
             if (newProfileName.equals(newProfileNameConfirmed))
             {
               currentUser.setName(newProfileName);
-              System.out.print("\n\nSuccess! Your profile name has been updated to: " + newProfileName + "\n\n----------------------------------------\nReturning to Update Menu");
+              profileChangeRequested = true;
+              System.out.print("\n\nSuccessful request to change profile name to " + newProfileName + ".\nYou must save your changes in the profile main menu to continue."
+                + "\n\n----------------------------------------\nReturning to Update Menu");
               FitnessAssistant.timedClearScreen();
             }
             else
@@ -79,9 +96,8 @@ public class Profile
               System.out.print("\n\nSorry, profile name could NOT be updated.\n\n------------------------\nReturning to Update Menu");
               FitnessAssistant.timedClearScreen();
             }
-
           }
-          // (2) "Update User Profile", (2) "Update Age"
+          /////////////////////////       (2) "Update User Profile", (2) "Update Age"     /////////////////////////
           else if (updateProfileInput == 2)
           {
             FitnessAssistant.clearScreen();
@@ -94,7 +110,9 @@ public class Profile
               if (!(newProfileAge <= 2 || newProfileAge > 120))
               {
                 currentUser.setAge(newProfileAge);
-                System.out.print("\n\nSuccess! Your profile age has been updated to: " + newProfileAge + "\n\n----------------------------------------\nReturning to Update Menu");
+                profileChangeRequested = true;
+                System.out.print("\n\nSuccessful request to change profile age to " + newProfileAge + ".\nYou must save your changes in the profile main menu to continue."
+                  + "\n\n----------------------------------------\nReturning to Update Menu");
                 FitnessAssistant.timedClearScreen();
               }
               else
@@ -109,9 +127,8 @@ public class Profile
               System.out.print("\n\nSorry, Invalid input provided.\n\n------------------------\nReturning to Update Menu");
               FitnessAssistant.timedClearScreen();
             }
-
           }
-          // (2) "Update User Profile", (3) "Update Height"
+          /////////////////////////       (2) "Update User Profile", (3) "Update Height"     /////////////////////////
           else if (updateProfileInput == 3)
           {
             FitnessAssistant.clearScreen();
@@ -142,8 +159,10 @@ public class Profile
                   else
                   {
                     currentUser.setHeight((newUserHeightFeet * 12) + newUserHeightInches);
+                    profileChangeRequested = true;
                     System.out.print(currentUser.getHeightCompleteString() + " makes you " + currentUser.getHeightInches() + " inches tall.\n\n"
-                      + "Your new height has been updated.\n\n----------------------------------------\nReturning to Update Menu");
+                      + "Successful request to change profile height.\nYou must save your changes in the profile main menu to continue."
+                      + "\n\n----------------------------------------\nReturning to Update Menu");
                     FitnessAssistant.timedClearScreen();
                   }
                 }
@@ -161,7 +180,7 @@ public class Profile
             }
           }
 
-          // (2) "Update User Profile", (4) "Update Lifestyle"
+          /////////////////////////       (2) "Update User Profile", (4) "Update Lifestyle"     /////////////////////////
           else if (updateProfileInput == 4)
           {
             FitnessAssistant.clearScreen();
@@ -178,7 +197,9 @@ public class Profile
               if (!(newProfileLifestyle <= 0 || newProfileLifestyle > 5))
               {
                 currentUser.setLifestyle(newProfileLifestyle);
-                System.out.print("\n\n\tSuccess! Your profile lifestyle has been updated to: Option " + newProfileLifestyle + "\n\n----------------------------------------\nReturning to Update Menu");
+                profileChangeRequested = true;
+                System.out.print("\n\n\tSuccessful request to change profile lifestyle to: Option " + newProfileLifestyle + ".\n\tYou must save your changes in the profile main menu to continue."
+                  + "\n\n----------------------------------------\nReturning to Update Menu");
                 FitnessAssistant.timedClearScreen();
               }
               else
@@ -194,10 +215,24 @@ public class Profile
               FitnessAssistant.timedClearScreen();
             }
           }
-          // (2) "Update User Profile", (9) "Save Changes and Exit"
+          /////////////////////////       (2) "Update User Profile", (9) "Save Changes and Exit"     /////////////////////////
           else if (updateProfileInput == 9)
           {
-            userDatabase.updateProfileDatabase();
+            if (profileChangeRequested)
+            {
+              userDatabase.updateProfileDatabase();
+              profileChangeRequested = false;
+            }
+            continueProgram = true;
+            continueUpdateProfile = false;
+          }
+          /////////////////////////       (2) "Update User Profile", (10) "Exit Without Saving Changes"     /////////////////////////
+          else if ((profileChangeRequested) && updateProfileInput == 10)
+          {
+            userDatabase.clearLocalMemory();
+            currentUser = userDatabase.getReloadedProfile(userProfileIndex);
+
+            profileChangeRequested = false;
             continueProgram = true;
             continueUpdateProfile = false;
           }
@@ -212,13 +247,16 @@ public class Profile
       input = -1;
       FitnessAssistant.clearScreen();
       }
+      /***********************************************************************|
+      |                       (9) "Exit Application"                          |
+      |***********************************************************************/
       else if (input == 9)
       {
         FitnessAssistant.clearScreen();
         continueProgram = false;
         System.out.print("\n\nThank you for using The Fitness Assistant!\nWritten by Eric S McDaniel, July-August 2018.\n\n");
       }
-
+      // If the user inputs any integer not listed in the main menu
       else
       {
         FitnessAssistant.clearScreen();
