d59c26f5e5e84b9b1ffdfa253200ddf43cbd1516
Eric McDaniel <McDanielES@gmail.com>
2018-08-06 01:01:27 -0500

    Add Basic User info before statistical analysis

--- UserProgress.java
+++ UserProgress.java
@@ -12,11 +12,13 @@ public class UserProgress
 
   public void updatedBMRStart()
   {
-    System.out.print("\n\t     _____________\n\t    | Progression |\n----------------------------------------\nWeight:\t\t"
+    System.out.print(printUserInfo());
+
+    System.out.print("\n\t     ____________\n\t    | Statistics |\n             ‾‾‾‾‾‾‾‾‾‾‾‾\nNew Weight:\t"
       + currentUser.getWeight() + " lbs.   ⇨    " + getNewWeight() + " lbs.\n   ↳  Difference:   "
       + Math.abs(currentUser.getWeight() - getNewWeight()) + " lbs. \n\nSince Start:\t" + currentUser.getStartingWeight()
       + " lbs.   ⇨    " + getNewWeight() + " lbs.\n   ↳  Gross Difference:   "
-      + Math.abs(currentUser.getStartingWeight() - getNewWeight()) + " lbs.\n\n\t\t/-----\\\nPercent Change:\t"
+      + Math.abs(currentUser.getStartingWeight() - getNewWeight()) + " lbs.\n\n\t     /‾‾‾‾‾‾‾‾‾‾‾\\\nPercent Change:\t"
       + percentChange(currentUser.getWeight()) + "% " + direction(currentUser.getWeight()) + " from "
       + currentUser.getWeight() + " lbs.\nSince Start:\t" + percentChange(currentUser.getStartingWeight()) + "% "
       + direction(currentUser.getStartingWeight()) + " from " + currentUser.getStartingWeight() + " lbs.");
@@ -34,6 +36,26 @@ public class UserProgress
     if (newWeight < _weight)
       return "decrease";
     return "increase";
+  }
+
+  private String profileDuration()
+  {
+    int hoursSinceStart = (int) ((((currentUser.getLastLogin() - currentUser.getJoined()) / 1000) / 60) / 60);
+    if (hoursSinceStart < 23)
+      return (hoursSinceStart + " hours");
+    else
+    {
+      if (hoursSinceStart == 1)
+        return ((hoursSinceStart / 24) + " day and " + (hoursSinceStart % 24) + " hours");
+      else
+        return ((hoursSinceStart / 24) + " days and " + (hoursSinceStart % 24) + " hours");
+    }
+  } // End String getLastLogin()
+  public String printUserInfo()
+  {
+    return ("___________________________________________\nName:\t\t" + currentUser.getName() + "\nAge:\t\t"
+      + currentUser.getAge() + " years old\nStart Date:\t" + currentUser.getJoinedFullPrint() + "\nDuration:\t"
+      + profileDuration() + "\n‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n");
   }
 
 }
